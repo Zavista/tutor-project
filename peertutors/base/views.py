f3954d77e8df6bfd881617cf1136788269ec5a62
Zavista <84294353+Zavista@users.noreply.github.com>
2024-10-15 17:29:08 -0400

    templates setup

--- peertutors/base/views.py
+++ peertutors/base/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+rooms = [
+    {'id': 1, 'name': 'Lets learn math!'},
+    {'id': 2, 'name': 'Lets learn english!'},
+    {'id': 3, 'name': 'Lets learn chemistry!'},
+]
+
 
 def home(request): 
-    return  HttpResponse('Home Page')
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
 
-def room(request):
-    return HttpResponse('ROOM')
+def room(request, id):
+    room = None
+    for i in rooms:
+        if i['id'] == int(id):
+            room = i
+
+    context = {'room': room}
+    return render(request, 'base/room.html', context)

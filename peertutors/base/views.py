865c2f8c0f200db79f7f6f606331930a50ee0566
Zavista <84294353+Zavista@users.noreply.github.com>
2024-10-16 18:13:53 -0400

    database setup

--- peertutors/base/views.py
+++ peertutors/base/views.py
@@ -1,21 +1,48 @@
-from django.shortcuts import render
-
-rooms = [
-    {'id': 1, 'name': 'Lets learn math!'},
-    {'id': 2, 'name': 'Lets learn english!'},
-    {'id': 3, 'name': 'Lets learn chemistry!'},
-]
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
 
 
 def home(request): 
+    rooms = Room.objects.all()
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 def room(request, id):
-    room = None
-    for i in rooms:
-        if i['id'] == int(id):
-            room = i
-
+    room = Room.objects.get(id=id)
     context = {'room': room}
     return render(request, 'base/room.html', context)
+
+def createRoom(request):
+    form = RoomForm()
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def updateRoom(request, id):
+    room = Room.objects.get(id=id)
+    form = RoomForm(instance=room)
+    
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, id):
+    room = Room.objects.get(id=id)
+    context = {'obj': room}
+
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', context)
